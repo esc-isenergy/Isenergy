628bac4af0fb8aa5ff897af632d2c5fc7981c900
Svetlana <badrutdinovaso@mail.ru>
2013-10-07 17:32:24 +0400

    Света занялась макетами

---
+++ IsEnergy/IsEnergy/Controllers/MaquettesF16Controller.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IsEnergy;
+using IsEnergyModel;
+using DevExpress.Web.ASPxUploadControl;
+
+namespace IsEnergy.Controllers
+{
+    public class MaquettesF16Controller : Controller
+    {
+        //
+        // GET: /MaquettesF16/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
